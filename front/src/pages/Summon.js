0e0e0d390e209e042fc35a70c15a8a23023c1121
Lucas Mastrovito <39515887+LucasMastrovito@users.noreply.github.com>
2025-08-13 17:32:33 +0200

    chakirock

--- front/src/pages/Summon.js
+++ front/src/pages/Summon.js
@@ -4,18 +4,20 @@ import axios from 'axios';
 
 function Summon() {
     const [Data, setData] = useState({});
+    const [Url, setUrl] = useState('chakirock');
 
     const summon = async (e) => {
         await axios.get('http://localhost:3000/summon').then((response => {
             setData(response.data);
-            console.log(Data)
+            console.log(Data.id)
+            setUrl(Data.id);
         }));
     }
 
     return (
     <div className="summon_container">
          <div className='summon_card'>
-            <img alt="cat" url='https://raw.githubusercontent.com/LucasMastrovito/GaChat/main/gifs/chapakonten.gif'></img>
+            <img className="gif" alt="cat" src={'https://raw.githubusercontent.com/LucasMastrovito/GaChat/main/public/' + {Url} + '.gif'}></img>
          </div>
         <button className="btn" onClick={summon}>Invoquer</button>
     </div>
