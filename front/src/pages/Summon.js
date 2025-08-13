8e307d9ab843704bbbdc1506c81468ca838d6c04
Lucas Mastrovito <39515887+LucasMastrovito@users.noreply.github.com>
2025-08-13 16:58:11 +0200

    gifs

--- front/src/pages/Summon.js
+++ front/src/pages/Summon.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import "./Summon.css";
+import axios from 'axios';
 
 function Summon() {
+    const [Data, setData] = useState({});
+
+    const summon = async (e) => {
+        await axios.get('http://localhost:3000/summon').then((response => {
+            setData(response.data);
+            console.log(Data)
+        }));
+    }
+
     return (
     <div className="summon_container">
-         <div className='summon_card'></div>
-        <button className="btn">Invoquer</button>
+         <div className='summon_card'>
+            <img alt="cat" url='https://c.tenor.com/sV0JRnsrjsUAAAAd/tenor.gif'></img>
+         </div>
+        <button className="btn" onClick={summon}>Invoquer</button>
     </div>
     )
 }
