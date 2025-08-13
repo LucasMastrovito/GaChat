666c12a453b84e6e75771d8b4433bf668a96171f
Lucas Mastrovito <39515887+LucasMastrovito@users.noreply.github.com>
2025-08-13 17:47:45 +0200

    gifs

--- front/src/pages/Summon.js
+++ front/src/pages/Summon.js
@@ -1,23 +1,25 @@
 import { useState } from "react";
 import "./Summon.css";
-import axios from 'axios';
 
 function Summon() {
     const [Data, setData] = useState({});
-    const [Url, setUrl] = useState('chakirock');
+    const [Url, setUrl] = useState('');
 
+    console.log(Url)
     const summon = async (e) => {
-        await axios.get('http://localhost:3000/summon').then((response => {
-            setData(response.data);
-            console.log(Data.id)
-            setUrl(Data.id);
-        }));
+        fetch('http://localhost:3000/summon')
+        .then(res => res.json())
+        .then(data => {
+            setData(data);
+            console.log(data.id)
+            setUrl(data.id.toLowerCase());
+        });
     }
 
     return (
     <div className="summon_container">
          <div className='summon_card'>
-            <img className="gif" alt="cat" src={'https://raw.githubusercontent.com/LucasMastrovito/GaChat/main/public/' + {Url} + '.gif'}></img>
+            <img className="gif" alt="cat" src={'https://raw.githubusercontent.com/LucasMastrovito/GaChat/main/public/' + Url + '.gif'}></img>
          </div>
         <button className="btn" onClick={summon}>Invoquer</button>
     </div>
