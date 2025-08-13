18739d043054a29c14589d673838d3611cfc406e
Lucas Mastrovito <39515887+LucasMastrovito@users.noreply.github.com>
2025-08-13 15:48:21 +0200

    front

---
+++ front/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Summon from './pages/Summon';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>GaChat !</h1>
+      </header>
+      <main>
+       <Summon></Summon>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/pages/Summon.js
@@ -0,0 +1,12 @@
+import "./Summon.css";
+
+function Summon() {
+    return (
+    <div className="summon_container">
+         <div className='summon_card'></div>
+        <button className="btn">Invoquer</button>
+    </div>
+    )
+}
+
+export default Summon;
