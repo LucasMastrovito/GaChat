5f2fceb7f0dd951f3c804e670bc63b80f77f362f
Lucas Mastrovito <39515887+LucasMastrovito@users.noreply.github.com>
2026-07-09 02:25:05 +0200

    Update Summon.js

--- front/src/pages/Summon.js
+++ front/src/pages/Summon.js
@@ -7,37 +7,37 @@ function Summon() {
     const [Url, setUrl] = useState('');
     const [Newgif, setNewgif] = useState(false);
     const [attemps, setAttemps] = useState(null);
-    const [allAttemps, setAllAttemps] = useState({rare: 0, mythic: 0, legendary: 0});
+    const [allAttemps, setAllAttemps] = useState({ rare: 0, mythic: 0, legendary: 0 });
     const [animKey, setAnimKey] = useState(0);
     const [animStill, setAnimStill] = useState(false);
 
     useEffect(() => {
         const get = async () => {
-            const res = await fetch('https://gachat.onrender.com/attemps/' +  localStorage.getItem('userId'));
+            const res = await fetch('https://gachat.onrender.com/attemps/' + localStorage.getItem('userId'));
             const data = await res.json();
 
             setAttemps(data.basic);
             setAllAttemps(data);
         }
         get();
-            
+
     }, [animStill]);
 
     const summon = async (type) => {
         fetch('https://gachat.onrender.com/summon/' + type.target.value + '/' + localStorage.getItem('userId'))
-        .then(res => res.json())
-        .then(data => {
-            setAnimStill(true);
-            if (data.hasOwnProperty('attemps')) {
-                setAttemps(data.basic);
-                setAllAttemps(data);
-            } else {
-                setData(data);
-                setUrl(data.id.toLowerCase());
-                setNewgif(data.newgif);
-                setAnimKey(animKey + 1);
-            }
-        });
+            .then(res => res.json())
+            .then(data => {
+                setAnimStill(true);
+                if (data.hasOwnProperty('attemps')) {
+                    setAttemps(data.basic);
+                    setAllAttemps(data);
+                } else {
+                    setData(data);
+                    setUrl(data.id.toLowerCase());
+                    setNewgif(data.newgif);
+                    setAnimKey(animKey + 1);
+                }
+            });
     };
 
     const summonComplete = () => {
@@ -46,47 +46,47 @@ function Summon() {
     }
 
     return (
-    <div className="summon_container">
-        { attemps === null ?
-        <h1>Chargement...</h1>
-        :
-        <div className="summon_card">
-            { Url ?
-            <div className="summon_gif">
-                <SummonAnimation
-                    key={animKey}
-                    rarity={Data.rarity} 
-                    gifUrl={'https://raw.githubusercontent.com/LucasMastrovito/GaChat/main/public/' + Url + '.gif'}
-                    name={Data.id}
-                    onComplete={summonComplete}
-                />
-                {/* <h2 className="newgif">{Newgif ? 'NEW !' : ''}</h2> */}
-            </div>
-             :
-            <div className="summon_gif">
-                <img alt="box" className="box" src="/abonnement.png" />
-            </div>
-            }
-            {!animStill && (
-                <div className="buttons-summon">
-                    { attemps === 0 ? <span></span> :
-                        <button className="btn" value="basic" onClick={summon}>Invoquer</button>
+        <div className="summon_container">
+            {attemps === null ?
+                <h1>Chargement...</h1>
+                :
+                <div className="summon_card">
+                    {Url ?
+                        <div className="summon_gif">
+                            <SummonAnimation
+                                key={animKey}
+                                rarity={Data.rarity}
+                                gifUrl={'https://raw.githubusercontent.com/LucasMastrovito/GaChat/main/public/' + Url + '.gif'}
+                                name={Data.id}
+                                onComplete={summonComplete}
+                            />
+                            <h2 className="newgif">{Newgif ? 'NEW !' : ''}</h2>
+                        </div>
+                        :
+                        <div className="summon_gif">
+                            <img alt="box" className="box" src="/abonnement.png" />
+                        </div>
                     }
-                    { allAttemps.rare <= 0 ? <span></span> :
-                        <button className="btn" value="rare" onClick={summon}>Invocation Rare</button>
-                    }
-                    { allAttemps.mythic <= 0 ? <span></span> :
-                        <button className="btn" value="mythic" onClick={summon}>Invocation Mythique</button>
-                    }
-                    { allAttemps.legendary <= 0 ? <span></span> :
-                        <button className="btn" value="legendary" onClick={summon}>Invocation Légendaire</button>
-                    }
-                    <h3>{attemps <= 0 ? "Tu n'as plus d'invocations..." : `Il te reste ${attemps} invocations !`}</h3>
+                    {!animStill && (
+                        <div className="buttons-summon">
+                            {attemps === 0 ? <span></span> :
+                                <button className="btn" value="basic" onClick={summon}>Invoquer</button>
+                            }
+                            {allAttemps.rare <= 0 ? <span></span> :
+                                <button className="btn" value="rare" onClick={summon}>Invocation Rare</button>
+                            }
+                            {allAttemps.mythic <= 0 ? <span></span> :
+                                <button className="btn" value="mythic" onClick={summon}>Invocation Mythique</button>
+                            }
+                            {allAttemps.legendary <= 0 ? <span></span> :
+                                <button className="btn" value="legendary" onClick={summon}>Invocation Légendaire</button>
+                            }
+                            <h3>{attemps <= 0 ? "Tu n'as plus d'invocations..." : `Il te reste ${attemps} invocations !`}</h3>
+                        </div>
+                    )}
                 </div>
-            )}
+            }
         </div>
-        }
-    </div>
     )
 }
 
