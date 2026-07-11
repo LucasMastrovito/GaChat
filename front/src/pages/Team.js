662350331978988a67f758921538e51a97c07123
Lucas Mastrovito <39515887+LucasMastrovito@users.noreply.github.com>
2026-07-11 19:49:20 +0200

    Update Team.js

--- front/src/pages/Team.js
+++ front/src/pages/Team.js
@@ -27,20 +27,25 @@ function Team(props) {
     return (
         <div className="team shadow" onClick={click}>
             <h2>{props.name}</h2>
-            <div className="teamCats">
-                <div>
-                    <img className="gif_collec shadow-gif" alt="cat" src={'https://raw.githubusercontent.com/LucasMastrovito/GaChat/main/public/' + props.cats[0].toLowerCase() + '.gif'}></img>
-                    <p>{props.cats[0]}</p>
+            {props.cats ?
+                <div className="teamCats">
+                    <div>
+                        <img className="gif_collec shadow-gif" alt="cat" src={'https://raw.githubusercontent.com/LucasMastrovito/GaChat/main/public/' + props.cats[0].toLowerCase() + '.gif'}></img>
+                        <p>{props.cats[0]}</p>
+                    </div>
+                    <div>
+                        <img className="gif_collec shadow-gif" alt="cat" src={'https://raw.githubusercontent.com/LucasMastrovito/GaChat/main/public/' + props.cats[1].toLowerCase() + '.gif'}></img>
+                        <p>{props.cats[1]}</p>
+                    </div>
+                    <div>
+                        <img className="gif_collec shadow-gif" alt="cat" src={'https://raw.githubusercontent.com/LucasMastrovito/GaChat/main/public/' + props.cats[2].toLowerCase() + '.gif'}></img>
+                        <p>{props.cats[2]}</p>
+                    </div>
                 </div>
-                <div>
-                    <img className="gif_collec shadow-gif" alt="cat" src={'https://raw.githubusercontent.com/LucasMastrovito/GaChat/main/public/' + props.cats[1].toLowerCase() + '.gif'}></img>
-                    <p>{props.cats[1]}</p>
-                </div>
-                <div>
-                    <img className="gif_collec shadow-gif" alt="cat" src={'https://raw.githubusercontent.com/LucasMastrovito/GaChat/main/public/' + props.cats[2].toLowerCase() + '.gif'}></img>
-                    <p>{props.cats[2]}</p>
-                </div>
-            </div>
+                :
+                <span />
+            }
+
             {props.canRemove ?
                 <button className="btn" onClick={remove}>Supprimer</button> :
                 <span />
